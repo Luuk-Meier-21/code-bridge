f5d5e5061df97ff514df70fc8ea79e041d7bdfd1
Luuk-Meier-21 <luuk.meier@outlook.com>
2022-09-30 11:14:27 +0200

    Initial Commit

---
+++ cb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cb
+//
+//  Created by Luuk Meier on 30/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cb/cbApp.swift
@@ -0,0 +1,17 @@
+//
+//  cbApp.swift
+//  cb
+//
+//  Created by Luuk Meier on 30/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct cbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
